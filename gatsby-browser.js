9af461ff65540c9954de6ececec119e6d6939f36
Marco Biedermann <marco.biedermann@gmx.de>
2019-08-12 08:34:44 +0200

    feat: Styles
    Add base styles

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,11 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/browser-apis/
- */
-
-// You can delete this file if you're not using it
+import './src/styles/all.css';
+import './src/styles/anchor.css';
+import './src/styles/body.css';
+import './src/styles/button.css';
+import './src/styles/figure.css';
+import './src/styles/headline.css';
+import './src/styles/html.css';
+import './src/styles/image.css';
+import './src/styles/input.css';
+import './src/styles/list.css';
+import './src/styles/paragraph.css';
