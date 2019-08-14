c0f4e31745316038fe824eeb5237a9841c654844
Marco Biedermann <marco.biedermann@gmx.de>
2019-08-14 20:17:07 +0200

    refactor: config
    Sort plugins and replace strings

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,41 +1,42 @@
 module.exports = {
   siteMetadata: {
-    title: `Performance Tooling Today`,
-    description: `Perf Tooling Today lists a lot of tools, articles, videos, slides and courses to make the web faster. We cover resources to automize and monitore your workflow.`,
-    author: `@stefanjudis`,
+    title: 'Performance Tooling Today',
+    description:
+      'Perf Tooling Today lists a lot of tools, articles, videos, slides and courses to make the web faster. We cover resources to automize and monitore your workflow.',
+    author: '@stefanjudis',
   },
   plugins: [
-    `gatsby-plugin-react-helmet`,
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sharp',
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: `images`,
+        name: 'images',
         path: `${__dirname}/src/images`,
       },
     },
-    `gatsby-transformer-json`,
+    'gatsby-transformer-json',
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
-        path: `./src/data/`,
+        path: './src/data/',
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    'gatsby-transformer-sharp',
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: 'gatsby-plugin-manifest',
       options: {
-        background_color: `#fff`,
-        display: `minimal-ui`,
-        icon: `src/images/favicon.jpg`,
-        name: `gatsby-starter-default`,
-        short_name: `perf-tooling`,
-        start_url: `/`,
-        theme_color: `#01a5b1`,
+        background_color: '#fff',
+        display: 'minimal-ui',
+        icon: 'src/images/favicon.jpg',
+        name: 'gatsby-starter-default',
+        short_name: 'perf-tooling',
+        start_url: '/',
+        theme_color: '#01a5b1',
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    // 'gatsby-plugin-offline',
   ],
 };
