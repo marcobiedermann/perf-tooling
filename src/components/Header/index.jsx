09d60b513bd284d041eb4f0357f16a06a2cd09e6
Marco Biedermann <marco.biedermann@gmx.de>
2019-08-18 18:15:05 +0200

    feat: header styles
    Add header styles

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,17 +1,22 @@
+import classNames from 'classnames';
 import PropTypes from 'prop-types';
 import React from 'react';
+import styles from './style.module.css';
 
 const Header = props => {
-  const { children } = props;
-  return <header>{children}</header>;
+  const { children, className } = props;
+
+  return <header className={classNames(className, styles.header)}>{children}</header>;
 };
 
 Header.propTypes = {
   children: PropTypes.node,
+  className: PropTypes.string,
 };
 
 Header.defaultProps = {
   children: null,
+  className: '',
 };
 
 export default Header;
