daed6b6729770b8e913b6693b7a30f3052468a78
Marco Biedermann <marco.biedermann@gmx.de>
2019-08-18 18:33:05 +0200

    feat: navigation styles
    Add navigation styles

--- src/components/Navigation/index.jsx
+++ src/components/Navigation/index.jsx
@@ -1,16 +1,20 @@
+import classNames from 'classnames';
 import { Link } from 'gatsby';
 import PropTypes from 'prop-types';
 import React from 'react';
+import styles from './style.module.css';
 
 const Navigation = props => {
-  const { routes } = props;
+  const { className, routes } = props;
 
   return (
-    <nav>
-      <ul>
+    <nav className={classNames(className, styles.navigation)}>
+      <ul className={styles.navigation__list}>
         {routes.map(route => (
           <li key={route.path}>
-            <Link to={route.path}>{route.name}</Link>
+            <Link to={route.path} className={styles.navigation__link} activeClassName={styles.navigation__linkActive}>
+              {route.name}
+            </Link>
           </li>
         ))}
       </ul>
@@ -19,6 +23,7 @@ const Navigation = props => {
 };
 
 Navigation.propTypes = {
+  className: PropTypes.string,
   routes: PropTypes.arrayOf(
     PropTypes.shape({
       name: PropTypes.string,
@@ -28,6 +33,7 @@ Navigation.propTypes = {
 };
 
 Navigation.defaultProps = {
+  className: '',
   routes: [],
 };
 
