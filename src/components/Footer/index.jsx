d17600be640a2d7344c256db75e23a6b189c37eb
Marco Biedermann <marco.biedermann@gmx.de>
2019-08-18 18:14:47 +0200

    feat: footer styles
    Add footer styles

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,16 +1,21 @@
+import classNames from 'classnames';
 import PropTypes from 'prop-types';
 import React from 'react';
+import styles from './style.module.css';
 
 const Footer = props => {
-  const { children } = props;
-  return <footer>{children}</footer>;
+  const { children, className } = props;
+
+  return <footer className={classNames(className, styles.footer)}>{children}</footer>;
 };
 
 Footer.propTypes = {
+  className: PropTypes.string,
   children: PropTypes.node,
 };
 
 Footer.defaultProps = {
+  className: '',
   children: null,
 };
 
